07f7d46c6f8be3251a9e02765b5b0bf66e22d944
Vy Quốc Vũ <vyquocvu@gmail.com>
2015-11-12 11:38:08 +0000

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+
+  def start_now
+    if logged_in?
+      redirect_to current_user
+    else
+      redirect_to signup_path
+    end
+  end
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
+end
